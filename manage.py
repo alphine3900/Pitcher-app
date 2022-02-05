38e0da3c1676cfcd100660c77d2d83d8000a4b95
alphine3900 <alphine.kirimi@student.moringaschool.com>
2022-02-06 01:41:51 +0300

    modified home page

--- manage.py
+++ manage.py
@@ -2,8 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/home')
+def home():
+  return render_template('home.html')
 
 
-     return render_template('index.html')
+
+@app.route('/register')
+def register():
+  return render_template('register.html')
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+
+if __name__ == '__main__':
+     app.run(debug = True) 
