76c5f7a1be484550fc1ae85decd7621b03e46dc5
alphine3900 <alphine.kirimi@student.moringaschool.com>
2022-02-06 21:15:46 +0300

    added primary key

--- manage.py
+++ manage.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import false
+# from flask_login import UserMixim
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://moringa:Access@localhost/alphine"
+app.config["SECRET_KEY"] = "thisisasecretkey"
+
+
+class User(db.model):
+  id = db.Column(db.Integer,primary_key=True)
+  User = db.Column(db.string(20), nullable=false)
+  password = db.Column(db.string(80), nullable=false)
+
 
 @app.route('/')
 def home():
