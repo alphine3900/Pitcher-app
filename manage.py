c554623b027212c7b11730063154d89ec338c49f
alphine3900 <alphine.kirimi@student.moringaschool.com>
2022-02-07 23:06:16 +0300

    added routes in the manage.py

--- manage.py
+++ manage.py
@@ -7,55 +7,67 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://moringa:Access@localhost/alphine"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 app.config["SECRET_KEY"] = "thisisasecretkey"
 
 
-class User(db.model):
+class User(db.Model):
   id = db.Column(db.Integer,primary_key=True)
-  User = db.Column(db.string(20), nullable=false)
-  completed = db.Column(db.integer,default=0)
+  User = db.Column(db.String(20), nullable=False)
+  completed = db.Column(db.Integer,default=0)
 
 
-@app.route('/',methods = ["GET","POST"])
-def home():
+@app.route('/login',methods = ["GET","POST"])
+def login():
 
   if request.method == "POST":
         session.pop ("user",None)
 
         if request.form["password"] == "password":
             session["user"] = request.form["username"]
-            return redirect(url_for("register"))
+            return redirect(url_for("home"))
 
-  return render_template('home.html')
+  return render_template('login.html')
+
 
 
 
 @app.route("/register")
 def register():
-    if g.user:
-        return render_template("protected.html",user=session["user"])
-        return redirect(url_for("register.html"))
+    if User:
+        return render_template("register.html")
+        return redirect(url_for("home"))
+
+
+
+
+@app.route("/")
+def home():
+    if User:
+        return render_template("home.html")
+        return redirect(url_for("register"))
 
 
 @app.before_request
 def before_request():
 
-    if g.user == None
+    if User == None:
 
-    if "user" in session:
-        g.user = session["user"]
+     if "user" in session:
+        user = session["user"]
 
-    return render_template("home.html")
+    return render_template("login.html")
+    return redirect(url_for("home"))
 
 
 
-@app.route('/register')
-def register():
-  return render_template('register.html')
+# @app.route('/register')
+# def register():
+#   return render_template('register.html')
 
-@app.route('/login')
-def login():
-  return render_template('login.html')
+# @app.route('/login')
+# def login():
+#   return render_template('login.html')
 
 
 if __name__ == '__main__':
