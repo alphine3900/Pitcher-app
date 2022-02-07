b2a99dcf2aa24725d1e7ffcb512275bd9e64cb6c
alphine3900 <alphine.kirimi@student.moringaschool.com>
2022-02-07 21:14:32 +0300

    added models in the manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+
+from crypt import methods
+from flask import Flask, session, render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import false
-# from flask_login import UserMixim
+
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -12,12 +13,39 @@ app.config["SECRET_KEY"] = "thisisasecretkey"
 class User(db.model):
   id = db.Column(db.Integer,primary_key=True)
   User = db.Column(db.string(20), nullable=false)
-  password = db.Column(db.string(80), nullable=false)
+  completed = db.Column(db.integer,default=0)
 
 
-@app.route('/')
+@app.route('/',methods = ["GET","POST"])
 def home():
+
+  if request.method == "POST":
+        session.pop ("user",None)
+
+        if request.form["password"] == "password":
+            session["user"] = request.form["username"]
+            return redirect(url_for("register"))
+
   return render_template('home.html')
+
+
+
+@app.route("/register")
+def register():
+    if g.user:
+        return render_template("protected.html",user=session["user"])
+        return redirect(url_for("register.html"))
+
+
+@app.before_request
+def before_request():
+
+    if g.user == None
+
+    if "user" in session:
+        g.user = session["user"]
+
+    return render_template("home.html")
 
 
 
